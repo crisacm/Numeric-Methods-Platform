9fb2cc140c62d70f91c3ccf11cab6cc311e763ba
crisacm <c.a.c.m997@gmail.com>
2019-11-21 01:28:12 -0500

    Last change... biseccion ux it's awesome! ^_^

--- application/__init__.py
+++ application/__init__.py
@@ -11,3 +11,23 @@ def not_found(error):
 @application.route('/')
 def main():
     return render_template('index.html')
+
+
+@application.route('/biseccion')
+def biseccion():
+    return render_template('biseccion.html')
+
+
+@application.route('/regla-falsa')
+def regla_falsa():
+    return render_template('regla-falsa.html')
+
+
+@application.route('/newton-rapshon')
+def newton_rapshon():
+    return render_template('newton-rapshon.html')
+
+
+@application.route('/secante')
+def secante():
+    return render_template('secante.html')
