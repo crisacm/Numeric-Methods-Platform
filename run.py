eb30bbab3dce4066b4e5b8878b5735ff75ad8d1a
crisacm <c.a.c.m997@gmail.com>
2019-11-26 23:17:48 -0500

    Last change..

--- run.py
+++ run.py
@@ -1,4 +1,20 @@
 from platform import platform
 
-platform.config['SECRET_KEY'] = "1hbd87sbwjV6stB#dj79726$!"
+class Config(object):
+    DEBUT = True
+    DEVELOPMENT = True
+    SECRET_KEY = "1hbd87sbwjV6stB#dj79726$!"
+    THREADS_PER_PAGE = 2
+    CSRF_ENABLED = True
+    # CSRF_SESSION_KEY = "1hbd87sbwjV6stB#dj79726$!"
+    # DB_HOST = 'local.database'
+
+
+class ProductionConfig(Config):
+    DEVELOPMENT = False
+    DEBUG = False
+    # DB_HOST = 'production.database'
+
+
+platform.config.from_object(Config())
 platform.run(debug=True)
